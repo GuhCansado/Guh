1008fb7103b32fae9cdcaf8852f5720ab39478ae
Gustavo <91337066+GuhCansado@users.noreply.github.com>
2024-12-10 10:42:08 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,18 @@
+// Função para mudar o fundo aleatoriamente
+window.onload = function() {
+    let imagens = ['IMG/imagem1.gif', 'IMG/imagem2.gif', 'IMG/imagem3.gif', 'IMG/imagem4.gif', 'IMG/imagem5.gif', 'IMG/imagem6.gif',]; // Adicione o caminho das suas imagens aqui
+    let randomImage = imagens[Math.floor(Math.random() * imagens.length)];
+    document.body.style.backgroundImage = `url(${randomImage})`;
+}
+
+// Abrir a página Spotify
+document.querySelector('.menu').addEventListener('click', function() {
+    document.getElementById('spotify-page').style.display = 'flex';
+});
+
+// Fechar a página Spotify
+document.getElementById('spotify-page').addEventListener('click', function(e) {
+    if (e.target === document.getElementById('spotify-page')) {
+        document.getElementById('spotify-page').style.display = 'none';
+    }
+});
