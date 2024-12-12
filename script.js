3a91fa320b1374cde40f254194150d05d27244bc
Gustavo <91337066+GuhCansado@users.noreply.github.com>
2024-12-12 02:53:49 -0300

    NEW BACKGRAUNDS
    NOVOS FUNDOS

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
 // Função para mudar o fundo aleatoriamente
 window.onload = function() {
-    let imagens = ['IMG/imagem1.gif', 'IMG/imagem2.gif', 'IMG/imagem3.gif', 'IMG/imagem4.gif', 'IMG/imagem5.gif', 'IMG/imagem6.gif','IMG/imagem7.gif','IMG/imagem7.gif','IMG/imagem8.gif','IMG/imagem9.gif',]; // Adicione o caminho das suas imagens aqui
+    let imagens = ['IMG/imagem1.gif', 'IMG/imagem2.gif', 'IMG/imagem3.gif', 
+        'IMG/imagem4.gif', 'IMG/imagem5.gif', 'IMG/imagem6.gif','IMG/imagem7.gif',
+        'IMG/imagem7.gif','IMG/imagem8.gif','IMG/imagem9.gif','IMG/imagem10.gif',
+        'IMG/imagem11.gif','IMG/imagem12.gif','IMG/imagem13.gif','IMG/imagem14.gif',
+        'IMG/imagem15.gif','IMG/imagem16.gif',]; // Adicione o caminho das suas imagens aqui
     let randomImage = imagens[Math.floor(Math.random() * imagens.length)];
     document.body.style.backgroundImage = `url(${randomImage})`;
 }
